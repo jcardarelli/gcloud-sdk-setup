fe5b9819c6b90fa1ffdbf86fb85b3ff347268354
John Cardarelli <stressed@desserts.yodel.space>
2018-05-21 00:01:18 -0700

    updated script with variables

--- gcloud-sdk-docker.sh
+++ gcloud-sdk-docker.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
+DOCKER_VOLUME="gcloud-config"
+DOCKER_IMAGE="google/cloud-sdk"
 
 # Install docker
 sudo apt-get install docker.io
 
 # Pull the Google Cloud SDK docker image from docker hub 
 # https://hub.docker.com/r/google/cloud-sdk
-docker pull google/cloud-sdk
+docker pull "$DOCKER_IMAGE"
 
 # Set these aliases in your ~/.profile for easy access to the Google Cloud SDK commands
-alias gcloud="docker run --rm -ti --volumes-from gcloud-config google/cloud-sdk gcloud"
-alias bq="docker run --rm -ti --volumes-from gcloud-config google/cloud-sdk bq"
-alias gsutil="docker run --rm -ti --volumes-from gcloud-config google/cloud-sdk gsutil"
-alias kubectl="docker run --rm -ti --volumes-from gcloud-config google/cloud-sdk kubectl"
+alias gcloud="docker run --rm -ti --volumes-from $DOCKER_VOLUME $DOCKER_IMAGE gcloud"
+alias bq="docker run --rm -ti --volumes-from $DOCKER_VOLUME $DOCKER_IMAGE bq"
+alias gsutil="docker run --rm -ti --volumes-from $DOCKER_VOLUME $DOCKER_IMAGE gsutil"
+alias kubectl="docker run --rm -ti --volumes-from $DOCKER_VOLUME $DOCKER_IMAGE kubectl"
 
 source ~/.profile
 
