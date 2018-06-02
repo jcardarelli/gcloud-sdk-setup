8d325025eb12af478749987dffedf26aa0c9e304
John Cardarelli <stressed@desserts.yodel.space>
2018-06-02 16:45:41 -0700

    Write alias commands to file variable

--- gcloud-sdk-docker.sh
+++ gcloud-sdk-docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 DOCKER_VOLUME="gcloud-config"
 DOCKER_IMAGE="google/cloud-sdk"
+FILE=$HOME/.profile
 
 # Install docker
 sudo apt-get install docker.io
@@ -10,12 +11,12 @@ sudo apt-get install docker.io
 docker pull "$DOCKER_IMAGE"
 
 # Set these aliases in your ~/.profile for easy access to the Google Cloud SDK commands
-alias gcloud="docker run --rm -ti --volumes-from $DOCKER_VOLUME $DOCKER_IMAGE gcloud"
-alias bq="docker run --rm -ti --volumes-from $DOCKER_VOLUME $DOCKER_IMAGE bq"
-alias gsutil="docker run --rm -ti --volumes-from $DOCKER_VOLUME $DOCKER_IMAGE gsutil"
-alias kubectl="docker run --rm -ti --volumes-from $DOCKER_VOLUME $DOCKER_IMAGE kubectl"
+echo "alias gcloud="docker run --rm -ti --volumes-from $DOCKER_VOLUME $DOCKER_IMAGE gcloud"" >> "$FILE"
+echo "alias bq="docker run --rm -ti --volumes-from $DOCKER_VOLUME $DOCKER_IMAGE bq"" >> "$FILE"
+echo "alias gsutil="docker run --rm -ti --volumes-from $DOCKER_VOLUME $DOCKER_IMAGE gsutil"" >> "$FILE"
+echo "alias kubectl="docker run --rm -ti --volumes-from $DOCKER_VOLUME $DOCKER_IMAGE kubectl"" >> "$FILE"
 
-source ~/.profile
+source "$FILE"
 
 # Run the cloud-sdk container with the "gcloud auth login" command to save your credentials in a docker volume
 gcloud auth login
